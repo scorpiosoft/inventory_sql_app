c7682323d3c62cdfe5f4d4a0fbdc051c3c379b27
Christopher Biessener <cpbiessener@gmail.com>
2018-02-16 14:41:46 -0600

    purchase choice works

--- customer.js
+++ customer.js
@@ -35,10 +35,17 @@ var connection = mysql.createConnection({
 //   runSearch();
 // });
 
+var product_list = [];
+
 function display_products()
 {
+  var do_add_list = false;
   var query = "SELECT item_id, product_name, price, department_name FROM products ORDER BY department_name";
   // var query = "SELECT item_id, product_name, price FROM products";
+
+  if (product_list.length === 0)
+    do_add_list = true;
+
   connection.query(query, function(err, res)
   {
     var dept, sav_dept = '';
@@ -56,10 +63,66 @@ function display_products()
                   , res[i].price
                 );
       console.log(out);
+      if (do_add_list) product_list.push(res[i].product_name);
+    }
+    // console.log(product_list);
+    console.log('');
+    make_purchase();
+  });
+}
+
+function check_inventory()
+{
+
+}
+
+function complete_purchase()
+{
+
+}
+
+    // name: "product",
+    // type: "input",
+    // message: "What would you like to buy?",
+    // validate: function(value)
+    // {
+    //   for (var i = 0; i < product_list.length; ++i)
+    //   {
+    //     if (value === product_list[i]) return true;
+    //   }
+    //   return false;
+    // }
+
+function make_purchase()
+{
+  inquirer.prompt([
+  {
+    name: "product",
+    type: "list",
+    message: "What would you like to buy?",
+    choices: product_list
+  },
+  {
+    name: "quantity",
+    type: "input",
+    message: "Quantity?",
+    validate: function(value)
+    {
+      if (isNaN(value)) return false;
+      return true;
+    }
+  }
+  ]).then(function(answers)
+  {
+    console.log(answers);
+    if (check_inventory(answers))
+    {
+      complete_purchase(answers);
     }
   });
 }
 
 display_products();
+// make_purchase();
 
 connection.end();
