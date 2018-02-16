f4e564fd0ffab6ca5000adbb4065a8c033e59fa7
Christopher Biessener <cpbiessener@gmail.com>
2018-02-16 10:22:11 -0600

    add seed data

---
+++ seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fuzzy Dice", "Novelties", 22.88, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fuzzy Mice", "Pets", 2.88, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Civilization", "Boardgames", 202.88, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fuzzy Bone", "Pets", 5.88, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Giant Foam Dice", "Novelties", 32.88, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("RPG Dice", "Boardgames", 12.88, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Insulated Mug", "Household", 15.88, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Oversized Rubber Bands", "Novelties", 0.88, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pet Bowl", "Pets", 6.88, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Untippable Mug", "Household", 19.88, 100);
